803419416a1d0cee4d79e33ac176381197792724
John Sloan <john.sloan@daveramsey.com>
2019-12-27 22:15:37 -0600

    fix: Lock mypy to less than 0.750.
    There was an API change that caused the typecats plugin to stop working.
    For now, you just have to lock mypy down.

--- cash.py
+++ cash.py
@@ -1,6 +1,33 @@
 from typecats import Cat
+import typing as ty
+
 
 @Cat
 class Denomination:
     value: int
 
+
+@Cat
+class Envelope:
+    name: str
+    denominations: ty.List[Denomination]
+
+
+@Cat
+class Budget:
+    month: int
+    year: int
+    envelopes: ty.List[Envelope]
+
+
+one = Denomination(value=1)
+envelope = Envelope(
+    name='Groceries',
+    denominations=[one]
+)
+
+jan20 = Budget(
+    month=1,
+    year=2020,
+    envelopes=[envelope]
+)
